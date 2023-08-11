aa272032e93f365770b05a17a0e317869ee75bf2
Beck <rebecca.pond@keyin.com>
2023-08-11 16:31:41 -0230

    option 8 report designed and explained in the comments  [KEEP THE COMMENTS IN THE PROGRAM]

--- Python/beck.py
+++ Python/beck.py
@@ -1,3 +1,4 @@
+"""
 f = open("Defaults.dat", "r")
 NEXT_TRANS_NUM = f.readline()
 DRIVER_NUM = int(f.readline())
@@ -41,3 +42,55 @@ print("Employee's insurance policy company:", InsPolComp)
 print("Employee's insurance policy number:", InsPolNum)
 print("Using own car:", OwnCar)
 print("Balance due:", BalDue)
+"""
+import datetime
+CurrDate = datetime.datetime.now()
+Date = CurrDate.strftime("%Y-%m-%d")
+Driver = "XXXXXXXXXXXXXXXXXXX"
+DriverNum = "####"
+Rating = "#/#" # rating is out of 5
+Review = "xxxxxxx..."
+# this is the driver rating table that will make it possible to track how employees are performing
+# for an exception report there would an if statement that if the drivers ratings are >= 3 then
+# they might need to be spoken to.
+print()
+print("HAB TAXI SERVICES")
+print(f"Driver Ratings as of: {Date}")
+print("-"*60)
+print("    DRIVER           DRIVER         RATING         REVIEW")
+print("     NAME            NUMBER          (1-5)")
+print("-"*60)
+print(f"{Driver}   {DriverNum}            {Rating}         {Review}")
+print(f"{Driver}   {DriverNum}            {Rating}         {Review}")
+print(f"{Driver}   {DriverNum}            {Rating}         {Review}")
+print(f"{Driver}   {DriverNum}            {Rating}         {Review}")
+print(f"{Driver}   {DriverNum}            {Rating}         {Review}")
+print(f"{Driver}   {DriverNum}            {Rating}         {Review}")
+print(f"{Driver}   {DriverNum}            {Rating}         {Review}")
+print(f"{Driver}   {DriverNum}            {Rating}         {Review}")
+print("-"*60)
+
+# this is the report for customer reviews, these would be inputted by drivers in order to keep track of
+# customers who may be a problem and refused service later on depending on their behaviour
+
+CustName = "XXXXXXXXXXXXXXXXX"
+CustRating = "#/#"
+CustReview = "xxxxxx..."
+CustPhone = "123-123-1234"
+
+print()
+print("HAB TAXI SERVICES")
+print(f"Customer Ratings As Of: {Date}") # Formatter here lol
+print("-"*61)
+print("   CUSTOMER         CUSTOMER        CUSTOMER        PHONE")
+print("    NAME             RATING          REVIEW         NUMBER")
+print("-"*61)
+print(f"{CustName}     {CustRating}           {CustReview}    {CustPhone}")
+print(f"{CustName}     {CustRating}           {CustReview}    {CustPhone}")
+print(f"{CustName}     {CustRating}           {CustReview}    {CustPhone}")
+print(f"{CustName}     {CustRating}           {CustReview}    {CustPhone}")
+print(f"{CustName}     {CustRating}           {CustReview}    {CustPhone}")
+print(f"{CustName}     {CustRating}           {CustReview}    {CustPhone}")
+print(f"{CustName}     {CustRating}           {CustReview}    {CustPhone}")
+print(f"{CustName}     {CustRating}           {CustReview}    {CustPhone}")
+print("-"*61)
