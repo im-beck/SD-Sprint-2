6384d45a63fac80efd8fd58708f3f0505b4aaf67
Austin Reid <Austin.Reid@keyin.com>
2023-08-12 21:54:52 -0230

    Updating files again for Car Tracking.

--- Final Project 2/CarRentals.py
+++ Final Project 2/CarRentals.py
@@ -8,11 +8,12 @@
 import Modules.Stylizer as S
 from Modules.Validizer import Validate as V
 import datetime as DT
+from datetime import timedelta
 
 # Constants
 Curr_Date = DT.datetime.now()
 Allowed_Characters = "0123456789"
-carLST = ["1","2","3","4"]
+carLST = ['1','2','3','4']
 
 # Inputs
 while True:
@@ -40,20 +41,51 @@ while True:
 
             startRental = V(V.date, "Enter the start date of the rental (YYYY-MM-DD): ")
             
-            carNum = (input("Enter the Car Number (1,2,3,4): "))
-            if carNum is not carLST():
-                 print("Error: Invalid entry, must provide car number 1-4.")
-                 continue
-            elif carNum != "":
-                 print("Error: Invalid entry, must provide car number.")
-                 continue
-            elif not all (char in Allowed_Characters for char in carNum):
-                 print("Error: Invalid entry, must provide number.")
-                 continue
-                 
+            while True:
+                carNum = (input("Enter the Car Number (1,2,3,4): "))
+                if carNum not in carLST:
+                    print("Error: Invalid entry, must provide car number 1-4.")
+                    continue
+                
+                rentTime = input("Is the Rental for a day or a week? (D/W): ").upper()
+                if rentTime == "":
+                    print("Error: Must input D for Day, W for a week.")
+                    continue
+                elif rentTime == "W":
+                    Week = 7
+                    print(Week)
+                elif rentTime == "D":
+                    dailyTime = Curr_Date.replace(hour=12, minute=0, second=0, microsecond=0)
+                    FormattedTime = dailyTime.strftime("%Y-%m-%d %H:%M")
+                    print(FormattedTime)
+                    break
 
-
-
+            while True:
+                rentCost = float(input("Enter the rental cost: "))
+                if rentCost == "":
+                    print("Error: Please enter the rental cost.")
+                    continue
+                elif not all (char in Allowed_Characters for char in rentCost):
+                    print("Error: Must only use integers.")
+                    continue
+                
+                taxCost = float(input("Enter the tax cost: "))
+                if taxCost == "":
+                    print("Error: Must enter a tax cost.")
+                    continue
+                elif not all (char in Allowed_Characters for char in taxCost):
+                    print("Error must only use integers.")
+                    continue
+                
+                totalCost = float(input("Enter the total cost: "))
+                if totalCost == "":
+                    print("Error: Must enter a total cost.")
+                    continue
+                elif not all (char in Allowed_Characters for char in totalCost):
+                    print("Error must only use integers.")
+                    continue
+                
+                
 # Calculations
 
 # Output
